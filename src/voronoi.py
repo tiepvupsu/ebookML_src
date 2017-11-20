b089b001a56f69b9355cde972d454e234f485073
tiepvupsu <vuhuutiep@gmail.com>
2017-11-20 00:36:02 -0500

    gd 1

--- src/voronoi.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-points = np.array([[1, 1], [8, 3], [6, 2]])
-from scipy.spatial import Voronoi, voronoi_plot_2d
-vor = Voronoi(points)
-
-import matplotlib.pyplot as plt
-voronoi_plot_2d(vor)
-plt.show()
